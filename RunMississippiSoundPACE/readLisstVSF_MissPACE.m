740e11a61d814568197b22f41c8a5b42c749cbb6
lucasbarbedo83 <lucas.barbedo@usm.edu>
2022-07-20 19:43:19 +0000

    Rrs, IOPs, and VSF simultaneous
    Stations are organized now, we can compare Rrs, LISSTVSF, and Rrs per stations.

--- RunMississippiSoundPACE/readLisstVSF_MissPACE.m
+++ RunMississippiSoundPACE/readLisstVSF_MissPACE.m
@@ -1,42 +1,79 @@
-function [ScatMatrix,PACE]=readLisstVSF_MissPACE(fname,ui)
+function [ScatMatrix,PACE]=readLisstVSF_MissPACE(fname,ui,FNlisstvsf)
 %fname='C:\Users\w10139248\Documents\Oceanography\NRL_USM_PACE_2022';
 % Read IOPS from Mississippi Sound PACE Mission
 %   Input
 %   fname: complet path to NRL_USM_PACE_2022
 %          for example, in my laptop:
 %          fname='C:\Users\w10139248\Documents\Oceanography\NRL_USM_PACE_2022'
-%   ui: 1 to select one file manually
-%         0 to run all PACE dataset
+%
+%   ui:    0 to run all PACE dataset
+%          1 to select one file manually
+%          2 to select the sequence of files to be read
+%
+%   FNlisstvsf: list of files names of LISSTVSF to be read.
+%   the processing PACE(n. station) respect the station order 
+%  it is good to evaluate dataset and cross multi-sensor validation.
+%
 %   Output
 %   lisstvsf: Mueller Matrix (VSF, P12, P21, P22, b, c, etc...)
 %   PACE: lisst vsf, inside a datab structured.
 %
 % Lucas Barbedo, 19 July 2022
 %% process DH4
- datab=[fname,'\IOP_data\PACE_lisstvsf_data_exfil'];
-if ui==1
+datab=[fname,'\IOP_data\PACE_lisstvsf_data_exfil'];
+if ui==0
+    str=[datab,'\V*.VSF'];
+    n=dir(str);
+elseif ui==1
     [filename, pathname] = uigetfile( ...
         {'*.VSF',  'IOPs (*.VSF)'}, ...
         'Pick a file', datab,...
         'MultiSelect', 'off');
     n(1).name=filename;
     n(1).folder=pathname;
-else
-    str=[datab,'\V*.VSF'];
-    n=dir(str);
+elseif ui==2
+    for i=1:length(FNlisstvsf)
+        n(i).name=char(FNlisstvsf(i));
+        n(i).folder=datab;
+    end
 end
 
 %% processing,...
 zsc=[datab,'\Z0331050.VSF'];
-for i=1:76%length(n)
-    disp(i)
+for i=1:length(n)%76%
     scatName=[n(i).folder,'\',n(i).name];
-    disp(scatName)
-    disp(zsc)
-    proc = lisstvsf_makep(1425,scatName,zsc,[],0) ;
-    % pause
-    PACE.proc=proc;
+    % disp(scatName)
+    if exist(scatName,'file')
+        proc = lisstvsf_makep(1425,scatName,zsc,[],0) ;
+        % pause
+        PACE(i).proc=proc;
+    else
+        PACE(i).proc.p11=nan;
+        proc=nan;
+    end
 
 end
 ScatMatrix=proc;
+
+
+
+figure
+axes('Box','on')
+hold on
+if length(n)>1
+    ccolor=parula(length(n));
+else
+    ccolor=[0 0 0];
+end
+for i=1:length(n)
+    if ~isnan( PACE(i).proc.p11)
+    semilogy(PACE(i).proc.angles,median(PACE(i).proc.p11,1),'-','Color',ccolor(i,:))
+    end
+end
+xlabel('$\theta$: scattering angle','Interpreter','latex')
+ylabel('$\beta(517)$: volume scattering function, in  m$^{-1}$ sr$^{-1}$','Interpreter','latex')
+hold off
+grid minor
+
+
 end
--- RunMississippiSoundPACE/readRrs_MissPACE.m
+++ RunMississippiSoundPACE/readRrs_MissPACE.m
@@ -1,30 +1,71 @@
-function [Rrs,PACE]=readRrs_MissPACE(fname,ui)
+function [Rrs,PACE]=readRrs_MissPACE(fname,ui,fieldnotes)
 % Read Rrs from Mississippi Sound PACE Mission
 %   Input
 %   fname: complet path to NRL_USM_PACE_2022
 %          for example, in my laptop:
 %          fname='C:\Users\w10139248\Documents\Oceanography\NRL_USM_PACE_2022'
-%   ui: 1 to select one file manually
-%         0 to run all PACE dataset
+%
+%   ui:  0 to run all PACE dataset
+%        1 to select one file manually
+%        2 to select the sequence of files to be read
+%
+%   FNradiometry: list of files names of above water radiometry
+%   the processing PACE(n. station) respect the station order
+%   it is good to evaluate dataset and cross multi-sensor validation.
+%
 %   Output
 %   Rrs: above water radiometry from selected file or last one
-%   PACE: above water inside a database structured.
+%   PACE: above water inside a datab structured.
 %
 % Lucas Barbedo, 19 July 2022
 
-database=[fname,'\RemoteSensingReflectance'];
-
-if ui
+datab=[fname,'\RemoteSensingReflectance'];
+if ui==0
+    str=[datab,'\*.csv'];
+    n=dir(str);
+elseif ui==1
     [filename, pathname] = uigetfile( ...
         {'*.csv',  'Rrs (*.csv)'}, ...
-        'Pick a file', database,...
+        'Pick a file', datab,...
         'MultiSelect', 'off');
     n(1).name=filename;
     n(1).folder=pathname;
-else
-    str=[database,'\*.csv'];
-    n=dir(str);
+elseif ui==2
+    %% the above water radiometry must be time and spatial coincident
+    % Stations based on IOPs
+    stationjd= datenum(fieldnotes.ObsStartTime);
+    stationlon=fieldnotes.Lon;
+    stationlat=fieldnotes.Lat;
+    % samples of Rrs
+    str=[datab,'\*.csv'];
+    samplesRrs=dir(str) ;
+    for niops=1:length(stationjd)
+        for nrrs=1:length(samplesRrs)
+            str=[samplesRrs(nrrs).folder,'\',samplesRrs(nrrs).name];
+            info = infoRrs_MissPACE(str);
+            jd=info.Value(8);
+            lat=info.Value(4);
+            lon=info.Value(5);
+            [dist,phaseangle] = sw_dist([lat stationlat(niops)],[ lon stationlon(niops)] ,'Km');
+            deltad(nrrs)=dist;
+            deltat(nrrs)=abs(stationjd(niops)-jd);
+            deltaVelo(nrrs)=dist./abs(stationjd(niops)-jd);
+        end
+
+        % [indD,posid]=min(deltad,2,'first');
+        % [indT,posit]=min(deltat);
+        [indVelo,posiVelo]=min(deltaVelo);
+
+        if deltad(posiVelo)<=1 && deltat(posiVelo)<=0.25 % 1 km and 6 hours.
+            n(niops).name=samplesRrs(posiVelo).name;
+            n(niops).folder=samplesRrs(posiVelo).folder;
+        else
+            n(niops).name='nan';
+            n(niops).folder=samplesRrs(posiVelo).folder;
+        end
+    end
 end
+
 opts = delimitedTextImportOptions("NumVariables", 13);
 % Specify range and delimiter
 opts.DataLines = [27, Inf];
@@ -39,10 +80,13 @@ opts.EmptyLineRule = "read";
 for i=1:length(n)
     %% Set up the Import Options and import the data
     str=[n(i).folder,'\',n(i).name];
-    disp(str)
-    Rrs  = readtable(str, opts); 
-    PACE(i).Rrs=Rrs;
-
+    if exist(str,'file')
+        Rrs  = readtable(str, opts);
+        PACE(i).Rrs=Rrs;
+    else
+        Rrs=nan;
+        PACE(i).Rrs=[];
+    end
     %% Clear temporary variables
 end
 clear opts
@@ -51,12 +95,14 @@ axes('YScale','log','YLim',[0.00001 0.1],'XLim',[350 1000],...
     'Box','on')
 hold on
 if length(n)>1
- ccolor=parula(length(n));
+    ccolor=parula(length(n));
 else
     ccolor=[0 0 0];
 end
 for i=1:length(n)
-plot(PACE(i).Rrs.wl,PACE(i).Rrs.MeanRrs,'-','Color',ccolor(i,:))
+    if ~isempty(PACE(i).Rrs)
+    plot(PACE(i).Rrs.wl,PACE(i).Rrs.MeanRrs,'-','Color',ccolor(i,:))
+    end
 end
 xlabel('$\lambda$: wavelength, in nm','Interpreter','latex')
 ylabel('$R_{rs}(\lambda)$: Remote sensing reflectance, in sr$^{-1}$','Interpreter','latex')
